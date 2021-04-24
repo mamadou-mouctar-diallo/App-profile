c69db0d2525c63b7f7148b2d3715b60bb2f8af3e
mamadou mouctar diallo <mamadoumouctardiallo256@yahoo.com>
2021-04-24 03:40:55 +0200

    refractor code and public/js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const path = require('path')
-
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log("Server starting..."))
+app.listen(PORT, () => console.log("Server starting... on port", PORT))
